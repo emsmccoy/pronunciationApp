12b13065502de52e8002880845166f386592f577
Emma <emmcoy7@gmail.com>
2025-01-22 19:49:41 +0100

    feat: connect to mock server

--- frontend/pronunciationAppFront/src/data-api.js
+++ frontend/pronunciationAppFront/src/data-api.js
@@ -1,12 +1,12 @@
 // api.js
 import axios from "axios";
 
-const BASE_URL = "https://a387bb02-2aa0-41a6-b8f7-9cc5247b9d5f.mock.pstmn.io";
+const BASE_URL = "https://8eeb858c-019e-4bd3-8287-f53ccc9400a7.mock.pstmn.io";
 
 export const fetchWords = async () => {
   try {
     const response = await axios.get(`${BASE_URL}/words`);
-    return response.data.words;
+    return response.data;
   } catch (error) {
     console.error("Error fetching words:", error);
     throw error;
