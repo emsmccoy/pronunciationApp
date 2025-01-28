786b1e9054d8220c238a4bee462bed802a4acecf
Emma <emmcoy7@gmail.com>
2025-01-28 12:58:19 +0100

    feat: create fetchUsers

--- frontend/pronunciationAppFront/src/data-api.js
+++ frontend/pronunciationAppFront/src/data-api.js
@@ -3,12 +3,23 @@ import axios from "axios";
 
 const BASE_URL = "https://8eeb858c-019e-4bd3-8287-f53ccc9400a7.mock.pstmn.io";
 
+
 export const fetchWords = async () => {
   try {
     const response = await axios.get(`${BASE_URL}/words`);
     return response.data;
   } catch (error) {
     console.error("Error fetching words:", error);
+    throw error;
+  }
+};
+
+export const fetchUsers = async () => {
+  try {
+    const response = await axios.get(`${BASE_URL}/users`);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching users:", error);
     throw error;
   }
 };
